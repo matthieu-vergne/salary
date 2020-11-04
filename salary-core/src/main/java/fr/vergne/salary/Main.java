dcf19bafd038d6c5fd9baa0a2091bb31850f4270
Matthieu Vergne <matthieu.vergne@gmail.com>
2020-11-04 22:48:23 +0100

    WIP

--- salary-core/src/main/java/fr/vergne/salary/Main.java
+++ salary-core/src/main/java/fr/vergne/salary/Main.java
@@ -1,18 +1,70 @@
 package fr.vergne.salary;
 
-import static fr.vergne.salary.Main.Profile.*;
 import static fr.vergne.salary.Main.Period.*;
+import static fr.vergne.salary.Main.Profile.*;
 import static fr.vergne.salary.Main.Statistics.*;
 import static java.util.stream.Collectors.*;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Map<Profile, Statistics> referenceData = createReferenceData();
+		Set<Profile> profiles = referenceData.keySet();
+
+		ModelGenerator modelGenerator = new ModelGenerator(new RandomGenerator(0));
+		Model referenceModel = modelGenerator.createDataBasedModel(referenceData);
+		System.out.println(referenceModel);
+
+		// TODO split profiles into single years of experience
+		// TODO split profiles into single years of seniority
+		double[] statisticsFactor = { 1 };
+		Map<Profile, Statistics> modelData = createFactoredData(referenceData,
+				(profile, statType) -> statisticsFactor[0]);
+		Model model = modelGenerator.createDataBasedModel(modelData);
+
+		// TODO Downhill algorithm to converge parameters to 1
+		// TODO Increase to 1 parameter per type
+		// TODO Increase to 1 parameter per profile & type
+		// TODO GA to converge parameters to reference
+		// TODO Parametered model on linear curves
+		// TODO Parametered model on exponential curves
+		// TODO Parametered model on logarithmic curves
+
+		int limitPerProfile = 100000;
+		Map<Profile, Collection<Double>> salaries = generateSalaries(profiles, model, limitPerProfile);
+
+		for (Profile profile : profiles.stream().sorted(bySeniorityThenExperience()).collect(toList())) {
+			Statistics profileData = referenceData.get(profile);
+			StatHelper profileResults = new StatHelper(salaries.get(profile));
+			System.out.println(profile);
+			displayDiff("Q1", profileResults.Q1(), profileData.Q1());
+			displayDiff("moy", profileResults.mean(), profileData.mean());
+			displayDiff("Q3", profileResults.Q3(), profileData.Q3());
+		}
+	}
+
+	private static Comparator<Profile> bySeniorityThenExperience() {
+		Comparator<Period> startComparator = Comparator.comparing(Period::start);
+		Comparator<Period> stopComparator = Comparator.comparing(Period::stop);
+		Comparator<Period> periodComparator = startComparator.thenComparing(stopComparator);
+
+		Comparator<Profile> seniorityComparator = Comparator.comparing(Profile::seniority, periodComparator);
+		Comparator<Profile> experienceComparator = Comparator.comparing(Profile::experience, periodComparator);
+		Comparator<Profile> profileComparator = seniorityComparator.thenComparing(experienceComparator);
+
+		return profileComparator;
+	}
+
+	private static Map<Profile, Statistics> createReferenceData() {
 		Period sen_0_1 = createPeriod(0, 1);
 		Period sen_2_5 = createPeriod(2, 5);
 
@@ -21,7 +73,7 @@ public class Main {
 		Period exp_6_9__ = createPeriod(6, 9);
 		Period exp_10_14 = createPeriod(10, 14);
 
-		Map<Profile, Statistics> data = Map.of(//
+		return Map.of(//
 				createProfile(sen_0_1, exp_1____), createStatistics(29.731, 34.062, 38.942), //
 				createProfile(sen_0_1, exp_2_5__), createStatistics(31.465, 35.797, 40.640), //
 				createProfile(sen_0_1, exp_6_9__), createStatistics(35.429, 40.476, 46.144), //
@@ -31,40 +83,53 @@ public class Main {
 				createProfile(sen_2_5, exp_2_5__), createStatistics(31.577, 35.863, 40.646), //
 				createProfile(sen_2_5, exp_6_9__), createStatistics(35.555, 40.551, 46.152), //
 				createProfile(sen_2_5, exp_10_14), createStatistics(40.107, 45.755, 52.090));
-		Set<Profile> profiles = data.keySet();
+	}
 
-		RandomGenerator rand = new RandomGenerator(0);
-		@SuppressWarnings("unused")
-		Model referenceModel = profile -> rand.gaussian(data.get(profile));
+	interface StatTypeFactor {
+		double get(Statistics.Type statType);
 
-		// TODO Parametered model equivalent to reference model
-		// TODO Identify optimal parameters
-		// TODO Downhill algorithm to converge parameters to reference
-		// TODO GA to converge parameters to reference
-		// TODO Parametered model on linear curves
-		// TODO Parametered model on exponential curves
-		// TODO Parametered model on logarithmic curves
-		Model model = profile -> rand.gaussian(data.get(profile));
-		int limitPerProfile = 100000;
-		Map<Profile, Collection<Double>> salaries = generateSalaries(profiles, model, limitPerProfile);
+		default double apply(Statistics.Type statType, Statistics reference) {
+			return get(statType) * statType.from(reference);
+		}
+	}
 
-		for (Profile profile : profiles) {
-			Statistics profileData = data.get(profile);
-			StatHelper profileResults = new StatHelper(salaries.get(profile));
-			System.out.println(profile);
-			displayDiff("Q1", profileResults.Q1(), profileData.Q1());
-			displayDiff("moy", profileResults.mean(), profileData.mean());
-			displayDiff("Q3", profileResults.Q3(), profileData.Q3());
+	interface StatFactor {
+		double get(Profile profile, Statistics.Type statType);
+
+		default StatTypeFactor onProfile(Profile profile) {
+			return statType -> get(profile, statType);
+		}
+	}
+
+	private static Map<Profile, Statistics> createFactoredData(Map<Profile, Statistics> data, StatFactor factor) {
+		return data.keySet().stream().collect(Collectors.toMap(//
+				profile -> profile, //
+				profile -> createFactoredStatistics(//
+						factor.onProfile(profile), //
+						data.get(profile))));
+	}
+
+	static class ModelGenerator {
+		private final RandomGenerator rand;
+
+		public ModelGenerator(RandomGenerator rand) {
+			this.rand = rand;
+		}
+
+		private Model createDataBasedModel(Map<Profile, Statistics> data) {
+			return profile -> rand.gaussian(data.get(profile));
 		}
 	}
 
 	private static Map<Profile, Collection<Double>> generateSalaries(Set<Profile> profiles, Model model,
 			int limitPerProfile) {
-		return profiles.stream().collect(toMap(//
-				profile -> profile, //
-				profile -> IntStream.range(0, limitPerProfile)//
-						.mapToObj(i -> model.estimateSalary(profile))//
-						.collect(toList())));
+		return profiles.stream()//
+				.sorted(bySeniorityThenExperience())// Order to generate same results with same random seed
+				.collect(toMap(//
+						profile -> profile, //
+						profile -> IntStream.range(0, limitPerProfile)//
+								.mapToObj(i -> model.estimateSalary(profile))//
+								.collect(toList())));
 	}
 
 	private static void displayDiff(String id, double actual, double target) {
@@ -129,7 +194,7 @@ public class Main {
 				public Period seniority() {
 					return seniority;
 				}
-				
+
 				@Override
 				public Period experience() {
 					return experience;
@@ -158,6 +223,20 @@ public class Main {
 	}
 
 	interface Statistics {
+		static enum Type {
+			Q1(Statistics::Q1), MEAN(Statistics::mean), Q3(Statistics::Q3);
+
+			private final Function<Statistics, Double> method;
+
+			private Type(Function<Statistics, Double> method) {
+				this.method = method;
+			}
+
+			public double from(Statistics reference) {
+				return method.apply(reference);
+			}
+		};
+
 		double Q1();
 
 		double mean();
@@ -180,6 +259,26 @@ public class Main {
 				@Override
 				public double Q3() {
 					return Q3;
+				}
+			};
+		}
+
+		static Statistics createFactoredStatistics(StatTypeFactor factor, Statistics reference) {
+			return new Statistics() {
+
+				@Override
+				public double Q1() {
+					return factor.apply(Type.Q1, reference);
+				}
+
+				@Override
+				public double mean() {
+					return factor.apply(Type.MEAN, reference);
+				}
+
+				@Override
+				public double Q3() {
+					return factor.apply(Type.Q3, reference);
 				}
 			};
 		}
